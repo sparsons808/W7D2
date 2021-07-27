f91b2b169beacdf57293f2f2b601f5a926e3d670
Sydney <sydneyparsonsyb@gmail.com>
2021-07-27 15:29:38 -0400

    users_controller

--- my_music_app/app/controllers/users_controller.rb
+++ my_music_app/app/controllers/users_controller.rb
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
+
+    render :show
   end
 
   # GET /users/new
@@ -19,33 +22,34 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @user = User.find(params[:id])
+    render :edit
   end
 
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
+    
       if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
+        login(@user)
+        redirect_to user_url(@user)
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        flash.now[:errors] = @user.errors.full_messages
+
+        render :new
       end
-    end
+    
   end
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to user_url(@user)
+    else
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
