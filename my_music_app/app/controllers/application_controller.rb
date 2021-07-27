71163b2ff52ff42f7db48435a3f7bb52726aa773
Sydney <sydneyparsonsyb@gmail.com>
2021-07-27 16:39:35 -0400

    application_controller might be wonky

--- my_music_app/app/controllers/application_controller.rb
+++ my_music_app/app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?, :log_in_user!
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login(user)
+        session[session_token] = user.reset_session_token!
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def ensure_logged_in
+        redirect_to new_session_url unless logged_in
+    end
+
+    def logout!
+        current_user.reset_session_token! if logged_in?
+        session[session_token] = nil
+        @current_user = nil
+    end
+
+    def ensure_logged_out
+        redirect_to users_url if logged_in?
+    end
+
+    def log_in_user!(user)
+        user.reset_session_token!
+        session[:session_token] = user.session_token
+    end
+
+
 end
--- my_music_app/app/controllers/sessions_controller.rb
+++ my_music_app/app/controllers/sessions_controller.rb
@@ -1,13 +1,26 @@
 class SessionsController < ApplicationController
-    def new
+    before_action :ensure_logged_in, only: [:destroy]
+    before_action :ensure_logged_out, only: [:new, :creates]
 
+    def new
+        @user = User.new
+        render :new
     end
 
     def create
-        
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user
+            login(@user)
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = ['Worng email or password']
+            render :show
+        end
     end
 
     def destroy
-
+        logout!
+        flash[:success] = ['Logged out']
+        redirect_to new_session_url
     end
 end
